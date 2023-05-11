e6fab26950d4b75dbbe9f8418e3fd6302b93392e
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-05-11 17:04:00 +0300

    LAB

--- testSol3/Program.cs
+++ testSol3/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!11111");
+            Console.WriteLine("Hellwseddedeo, World!11111");
         }
     }
 }
